9dd984fb37cf57c122f05bf3a0ca8bb05a60eb65
tanviriqbal <tanviriqbal4u@live.com>
2018-05-30 21:20:11 +0600

    Run C code in VS Code

---
+++ 0 Hello World.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!");
+}
+
+/*
+1. Install MinGW
+2. Add System Environment Path to your MinGW bin folder "C:\MinGW\bin"
+3. Check if installation is done successfully. Open cmd and run "g++ --version"
+4. Install c/c++ extension by Microsoft in VS Code
+5. Add c_cpp_properties.json
+6. Create a task.json file to compile the file
+7. ctrl + shift + B to build
+8. In the Terminal Run ".\a.exe"
+*/
